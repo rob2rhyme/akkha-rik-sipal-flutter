1dd90388513341d3d4a9709bb61cb0a4a55650bd
vivek2112 <vivek@benzatine.com>
2023-08-07 16:49:42 +0530

    video_player_screen bug solve

--- lib/ui/video_player/views/video_player_screen.dart
+++ lib/ui/video_player/views/video_player_screen.dart
@@ -15,229 +15,223 @@ class VideoPlayerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        Get.back();
-        return false;
-      },
-      child: GetBuilder<VideoPlayerController>(builder: (logic) {
-        return YoutubePlayerBuilder(
-          onExitFullScreen: () {
-            SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-                statusBarColor: AppColor.transparent));
+    return GetBuilder<VideoPlayerController>(builder: (logic) {
+      return YoutubePlayerBuilder(
+        onExitFullScreen: () {
+          SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+              statusBarColor: AppColor.transparent));
+        },
+        player: YoutubePlayer(
+          controller: logic.controller,
+          showVideoProgressIndicator: true,actionsPadding: EdgeInsets.all(10),
+          progressIndicatorColor: Colors.red,
+          topActions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  Get.back();
+                },
+                icon: Icon(Icons.arrow_back,color: AppColor.white,)),
+            const SizedBox(width: 8.0),
+            Expanded(
+              child: Text(
+                logic.videoTitle[logic.index],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ),
+          ],
+          onReady: () {
+            logic.isPlayerReady = true;
           },
-          player: YoutubePlayer(
-            controller: logic.controller,
-            showVideoProgressIndicator: true,actionsPadding: EdgeInsets.all(10),
-            progressIndicatorColor: Colors.red,
-            topActions: <Widget>[
-              IconButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  icon: Icon(Icons.arrow_back,color: AppColor.white,)),
-              const SizedBox(width: 8.0),
-              Expanded(
+          onEnded: (data) {
+            logic.controller.load(logic.videoIds[
+                (logic.videoIds.indexOf(data.videoId) + 1) %
+                    logic.videoIds.length]);
+            logic.showSnackBar('Next Video Started!', context);
+          },
+        ),
+        builder: (context, player) => Scaffold(
+          body: Column(
+            children: [
+              player,
+              Padding(
+                padding: const EdgeInsets.all(12.0),
                 child: Text(
                   logic.videoTitle[logic.index],
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: AppFontSize.size_14),
                 ),
               ),
-            ],
-            onReady: () {
-              logic.isPlayerReady = true;
-            },
-            onEnded: (data) {
-              logic.controller.load(logic.videoIds[
-                  (logic.videoIds.indexOf(data.videoId) + 1) %
-                      logic.videoIds.length]);
-              logic.showSnackBar('Next Video Started!', context);
-            },
-          ),
-          builder: (context, player) => Scaffold(
-            body: Column(
-              children: [
-                player,
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    logic.videoTitle[logic.index],
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: AppFontSize.size_14),
-                  ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    padding: EdgeInsets.zero,
-                    itemCount: logic.videoTitle.length,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          logic.changeVideo(index);
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            bottom: 10.0,
-                            right: 10,
-                            left: 10,
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: AppColor.white,
-                                borderRadius: BorderRadius.circular(12),
-                                boxShadow: const [
-                                  BoxShadow(
-                                      color: AppColor.colorGray50,
-                                      spreadRadius: 1,
-                                      offset: Offset(1.2, 0.5),
-                                      blurRadius: 5)
-                                ]),
-                            child: Row(children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: SizedBox(
-                                  height: AppSizes.height_10,
-                                  width: AppSizes.height_15,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(12),
-                                    child: PhotoView(
-                                      gaplessPlayback: true,
-                                      minScale:
-                                          PhotoViewComputedScale.covered * 1.35,
-                                      maxScale:
-                                          PhotoViewComputedScale.covered * 1.35,
-                                      imageProvider:
-                                          NetworkImage(logic.videoThumb[index]),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: AutoSizeText(
-                                    logic.videoTitle[index] ?? "",
-                                    textAlign: TextAlign.left,
-                                    // Center the text horizontally
-                                    style: TextStyle(
-                                        fontSize: AppFontSize.size_12,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ),
-                              ),
-                            ]),
-                          ),
+              Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount: logic.videoTitle.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        logic.changeVideo(index);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 10.0,
+                          right: 10,
+                          left: 10,
                         ),
-                      );
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-
-/*      return YoutubePlayerScaffold(
-          controller: logic.controller,
-          builder: (context, player) {
-            return Scaffold(
-              body: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Column(
-                    children: [
-                      player,
-                      const VideoPositionIndicator(),
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Text(
-                          logic.videoTitle[logic.index],
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: AppFontSize.size_14),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: AppColor.white,
+                              borderRadius: BorderRadius.circular(12),
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: AppColor.colorGray50,
+                                    spreadRadius: 1,
+                                    offset: Offset(1.2, 0.5),
+                                    blurRadius: 5)
+                              ]),
+                          child: Row(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                height: AppSizes.height_10,
+                                width: AppSizes.height_15,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: PhotoView(
+                                    gaplessPlayback: true,
+                                    minScale:
+                                        PhotoViewComputedScale.covered * 1.35,
+                                    maxScale:
+                                        PhotoViewComputedScale.covered * 1.35,
+                                    imageProvider:
+                                        NetworkImage(logic.videoThumb[index]),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: AutoSizeText(
+                                  logic.videoTitle[index] ?? "",
+                                  textAlign: TextAlign.left,
+                                  // Center the text horizontally
+                                  style: TextStyle(
+                                      fontSize: AppFontSize.size_12,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ),
+                          ]),
                         ),
                       ),
-                      Expanded(
-                        child: ListView.builder(
-                          padding: EdgeInsets.zero,
-                          itemCount: logic.videoTitle.length,
-                          itemBuilder: (context, index) {
-                            return InkWell(
-                              onTap: () {
-                               logic.changeVideo(index);
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.only(
-                                  bottom: 10.0,
-                                  right: 10,
-                                  left: 10,
-                                ),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: AppColor.white,
-                                      borderRadius: BorderRadius.circular(12),
-                                      boxShadow: const [
-                                        BoxShadow(
-                                            color: AppColor.colorGray50,
-                                            spreadRadius: 1,
-                                            offset: Offset(1.2, 0.5),
-                                            blurRadius: 5)
-                                      ]),
-                                  child: Row(children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: SizedBox(
-                                        height: AppSizes.height_10,
-                                        width: AppSizes.height_15,
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(12),
-                                          child: PhotoView(
-                                            gaplessPlayback: true,
-                                            minScale:
-                                                PhotoViewComputedScale.covered *
-                                                    1.35,
-                                            maxScale:
-                                                PhotoViewComputedScale.covered *
-                                                    1.35,
-                                            imageProvider: NetworkImage(
-                                                logic.videoThumb[index]),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: AutoSizeText(
-                                          logic.videoTitle[index] ?? "",
-                                          textAlign: TextAlign.left,
-                                          // Center the text horizontally
-                                          style: TextStyle(
-                                              fontSize: AppFontSize.size_14,
-                                              fontWeight: FontWeight.w700),
-                                        ),
-                                      ),
-                                    ),
-                                  ]),
-                                ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+
+/*      return YoutubePlayerScaffold(
+        controller: logic.controller,
+        builder: (context, player) {
+          return Scaffold(
+            body: LayoutBuilder(
+              builder: (context, constraints) {
+                return Column(
+                  children: [
+                    player,
+                    const VideoPositionIndicator(),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        logic.videoTitle[logic.index],
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: AppFontSize.size_14),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: logic.videoTitle.length,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () {
+                             logic.changeVideo(index);
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                bottom: 10.0,
+                                right: 10,
+                                left: 10,
                               ),
-                            );
-                          },
-                        ),
-                      )
-                    ],
-                  );
-                },
-              ),
-            );
-          },
-        );*/
-      }),
-    );
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: AppColor.white,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                          color: AppColor.colorGray50,
+                                          spreadRadius: 1,
+                                          offset: Offset(1.2, 0.5),
+                                          blurRadius: 5)
+                                    ]),
+                                child: Row(children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SizedBox(
+                                      height: AppSizes.height_10,
+                                      width: AppSizes.height_15,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(12),
+                                        child: PhotoView(
+                                          gaplessPlayback: true,
+                                          minScale:
+                                              PhotoViewComputedScale.covered *
+                                                  1.35,
+                                          maxScale:
+                                              PhotoViewComputedScale.covered *
+                                                  1.35,
+                                          imageProvider: NetworkImage(
+                                              logic.videoThumb[index]),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: AutoSizeText(
+                                        logic.videoTitle[index] ?? "",
+                                        textAlign: TextAlign.left,
+                                        // Center the text horizontally
+                                        style: TextStyle(
+                                            fontSize: AppFontSize.size_14,
+                                            fontWeight: FontWeight.w700),
+                                      ),
+                                    ),
+                                  ),
+                                ]),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
+          );
+        },
+      );*/
+    });
   }
 }
